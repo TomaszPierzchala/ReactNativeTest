20125b66fa0aab5d46552df016898adb66680387
Tomasz Pierzchała <t.pierzchala@gmail.com>
2022-08-03 15:25:18 +0200

    11. react-navigation : Stack.Navigator

--- App.js
+++ App.js
@@ -1,191 +1,104 @@
+import * as React from 'react';
+import { Button, View, Text, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React, { useState } from 'react';
+const onPressed = ({ pressed }) => ([{ backgroundColor: pressed ? '#aaa' : "aliceblue" },
+{ borderWidth: 1, borderRadius: 20 },
+]);
 
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Modal,
-  Image,
-  ImageBackground,
-} from 'react-native';
-import MyButtom from './src/CustomButton';
-import Header from './src/Header';
+const MyButton = (props) => {
 
-const App = () => {
-
-  const [name, setName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const [showModalWarning, setShowWarning] = useState(false);
-
-  const onPressHandler = () => {
-    if (name.length > 2) {
-      setSubmitted(!submitted);
-    } else {
-      setShowWarning(true)
-    }
-  }
   return (
-    <ImageBackground
-      style={styles.body}
-      source={{ uri: 'https://cdn.pixabay.com/photo/2013/07/12/12/35/texture-145968_960_720.png' }}
-    >
-      <Header/>
-      <Modal
-        visible={showModalWarning}
-        transparent
-        onRequestClose={() => setShowWarning(false)}
-        animationType='slide'
-        hardwareAccelerated
-      >
-        <View style={styles.modal_centered}>
-          <View style={styles.modal_warning}>
-            <View style={styles.warning_title}>
-              <Text style={styles.modal_text}>WARNING !</Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.modal_text}>The name must be longer then 2 characters</Text>
-            </View>
-            <Pressable style={styles.ok}
-              android_ripple={{ color: '#00f' }}
-              onPress={() => setShowWarning(false)}>
-              <Text style={styles.modal_text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>
-        Please insert your name:
-      </Text>
-      <TextInput style={styles.input}
-        placeholder='e.g. Tom'
-        onChangeText={(val) => setName(val)}
-        keyboardType='number-pad'
-      />
-      <MyButtom
-      onPressFunction={onPressHandler}
-      title={submitted ? 'Clear' : 'Submit'}
-      color='#0f0'
-      />
-      <MyButtom
-      onPressFunction={onPressHandler}
-      title={submitted ? 'Clear' : 'Submit'}
-      color='purple'
-      text_style={{color: '#eee'}}
-      style={{margin: 10}}
-      />
-      {/* <Pressable
-        onPress={onPressHandler}
-        style={
-          ({ pressed }) => [styles.button,
-          { backgroundColor: pressed ? 'grey' : '#0f0' }
-          ]
-        }
-        hitSlop={{ top: 10, bottom: 15 }}
+    <View style={{ marginBottom: 10 }}>
+      <Pressable
+        onPress={props.goto}
+        style={onPressed}
       >
         <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
+          {props.name}
         </Text>
-      </Pressable> */}
-      {
-        submitted ?
-          <View style={styles.body}>
-            <Text style={styles.text}>
-              You typed : {name}
-            </Text>
-            <Image
-              style={styles.image_done}
-              source={require('./assets/done.png')}
-            // resizeMode='stretch'
-            />
-          </View>
-          :
-          <Image
-            style={styles.image_error}
-            source={require('./assets/error.png')}
-            resizeMode='stretch'
-            blurRadius={2}
-          />
-      }
-    </ImageBackground >
+      </Pressable>
+    </View>
   )
-};
+}
+
+function HomeScreen( {navigation} ) {
+  return (
+    <View style={styles.body}>
+      <MyButton name={'Go to Profile'} goto={() => navigation.navigate('Profile')} />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) { // <<< important "navigation" prop ! <<<
+  return (
+    <View style={styles.body}>
+      <View>
+        <MyButton name={'Go to Notifications'} goto={() => navigation.navigate('Notifications')} />
+      </View>
+      <View>
+        <MyButton name={'Go back'} goto={() => navigation.goBack()} />
+      </View>
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={styles.body}>
+      <View>
+        <MyButton name={'Go to Settings'} goto={() => navigation.navigate('Settings')} />
+      </View>
+      <View>
+        <MyButton name={'Go back'} goto={() => navigation.goBack()} />
+      </View>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={styles.body}>
+      <MyButton name={'Go back'} goto={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator
+      // screenOptions={{header: ()=>null}} // removes heder if needed
+    >
+      <Stack.Screen name="Home" component={HomeScreen} 
+        // options={{header: ()=>null}} // removes heder if needed
+      />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default App = () => {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-
   text: {
-    color: 'green',
-    fontSize: 25,
-    fontStyle: 'italic',
+    fontSize: 30,
+    fontWeight: 'bold',
     margin: 10,
-  },
-  modal_text: {
-    fontSize: 20,
-    fontStyle: 'italic',
-    margin: 5,
-    textAlign: 'center'
-  },
-  input: {
-    borderWidth: 1,
-    borderRadius: 20,
-    width: 220,
-    padding: 9,
-    margin: 10,
-  },
-  modal_centered: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0044ee55'
-  },
-  modal_warning: {
-    width: 300,
-    height: 200,
-    backgroundColor: '#0489',
-    borderWidth: 2,
-    borderColor: "#00a",
-    borderRadius: 40,
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  warning_title: {
-    height: 50,
-    width: 300,
-    backgroundColor: 'yellow',
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    justifyContent: 'center',
-  },
-  warning_body: {
-    height: 90,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  ok: {
-    width: 50,
-    height: 40,
-    backgroundColor: 'skyblue',
-    borderRadius: 15,
-  },
-  image_error: {
-    width: 80,
-    height: 72,
-    margin: 20,
-
-  },
-  image_done: {
-    width: 80,
-    height: 80,
-    margin: 20,
-
   }
-});
-
-export default App;
+})
